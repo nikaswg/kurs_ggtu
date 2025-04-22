c649fdbaab487dab46c0966f42cd6650b8353b01
Nikita Nagornuik <nikitanagornuik@gmail.com>
2025-04-22 03:25:58 +0300

    nachalo

---
+++ ClassLibrary1/MyDbContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Data.Linq;
+
+public class MyDbContext : DataContext
+{
+    public MyDbContext(string connectionString) : base(connectionString) { }
+    public Table<User> Users => GetTable<User>(); // Связь с таблицей Users
+}
---
+++ ClassLibrary1/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Linq.Mapping;
+
+[Table(Name = "Users")] // Указываем имя таблицы в БД
+public class User
+{
+    [Column] public string? Name { get; set; }
+    [Column] public string? Email { get; set; }
+    [Column(Name = "Password")] // Если в БД поле называется "Password", а не "PasswordHash"
+    public string? PasswordHash { get; set; }
+    [Column] public string? Role { get; set; }
+    [Column] public int? NameId { get; set; }
+}
